22df3c8b8776cf77108baa099fd29cc2590ddbb1
isak-iiii <t-yuta08780126@outlook.jp>
2022-05-25 02:59:57 +0900

    タイムラインの見た目を作成

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import Post from './Post'
 import "./Timeline.css"
+import TweetBox from './TweetBox.js'
 
 function Timeline() {
     return (
         <div className='timeline'>
             {/* Header */}
-            <div className='timelineHeader'></div>
-            <h2>ホーム</h2>
+            <div className='timeline_header'>
+                <h2>ホーム</h2>
+            </div>
             {/* TweetBox */}
+            <TweetBox />
             {/* Post */}
+            <Post />
         </div>
     )
 }
