0c8fd98277d3e1deea98b4e2d1a44fa8a2192dd9
isak-iiii <t-yuta08780126@outlook.jp>
2022-05-24 21:57:54 +0900

    タイムラインのベースを作成

---
+++ src/components/timeline/Timeline.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Timeline.css"
+
+function Timeline() {
+    return (
+        <div className='timeline'>
+            {/* Header */}
+            <div className='timelineHeader'></div>
+            <h2>ホーム</h2>
+            {/* TweetBox */}
+            {/* Post */}
+        </div>
+    )
+}
+
+export default Timeline
