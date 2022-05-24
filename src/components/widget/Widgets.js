8ed13a0fa45e0f05acb5f1b04add4a9eb5a264e8
isak-iiii <t-yuta08780126@outlook.jp>
2022-05-25 02:59:03 +0900

    ウィジェットコンポーネントの作成

---
+++ src/components/widget/Widgets.js
@@ -0,0 +1,23 @@
+import { Search } from '@mui/icons-material'
+import React from 'react'
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed } from 'react-twitter-embed';
+import "./Widgets.css"
+
+function Widgets() {
+    return (
+        <div className='widgets'>
+            <div className='widgets_input'>
+                <Search className='widgets_serchIcon' />
+                <input placeholder='キーワード検索' type="text"></input>
+            </div>
+            <div className='widgets_widgetContainer'>
+                <h2>いまどうしてる？</h2>
+                <TwitterTweetEmbed tweetId={'1529102712100769792'} />
+                <TwitterTimelineEmbed sourceType='profile' screenName='tan_tsubo01' options={{ height: 400 }} />
+                <TwitterShareButton url='https://twitter.com/tan_tsubo01' options={{ text: 'react.jsによるTwitter Clone', via: 'tan_tsubo01' }} />
+            </div>
+        </div>
+    )
+}
+
+export default Widgets
