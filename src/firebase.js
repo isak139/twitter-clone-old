6fdbb2a90a765b49b66b21bbb8f0fef454edb45a
isak-iiii <t-yuta08780126@outlook.jp>
2022-05-25 19:17:10 +0900

    import文 修正

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
@@ -11,4 +11,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore();
+const db = getFirestore(app);
+
+export default db;
